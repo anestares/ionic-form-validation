721cb4e8111121706e1e1bef8be7b8a0b06e698a
Aldo Nestares <anestares@gmail.com>
2021-02-27 09:25:28 -0500

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
-import { HomePageRoutingModule } from './home-routing.module';
-
+import { RouterModule } from '@angular/router';
+//import { HomePageRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    //HomePageRoutingModule,
+    RouterModule.forChild([
+      {
+      path: '',
+      component: HomePage
+      }
+    ])     
   ],
   declarations: [HomePage]
 })
